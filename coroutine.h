5e05351fa1b353aa890f859ebc4b43c313e994e2
mybluue <73818220+mybluue@users.noreply.github.com>
2022-02-27 11:52:24 +0800

    Update coroutine.h

--- coroutine.h
+++ coroutine.h
@@ -5,11 +5,11 @@
 #include <functional>
 #include <memory>
 
-#define DEFAULT_SIZE (1024*1024)
+#define DEFAULT_SIZE (1024*1024)        // 协程栈默认大小
 
 class Scheduler;
 
-class Coroutine :public std::enable_shared_from_this<Coroutine> {
+class Coroutine :public std::enable_shared_from_this<Coroutine> {       // 协程实例由智能指针管理时，安全地将this指针传递给外部
     friend class Scheduler;
 public:
     typedef std::shared_ptr<Coroutine> ptr;
@@ -27,16 +27,16 @@ public:
     State getState() {return m_state;}
     void yield();
     void resume();
-    static void corouFunc();
+    static void corouFunc();        // 协程入口函数，包裹回调函数m_cb
     static void setThis(Coroutine* cptr);      // 将this指针设置为正在运行的协程指针
     static Coroutine::ptr getThis();        // 获取正在运行的协程的this指针
     static void initCallerCorou();
 private:
-    ucontext_t m_ctx;
-    char m_stack[DEFAULT_SIZE];
-    State m_state;
-    int m_id;
-    std::function<void()> m_cb;
+    ucontext_t m_ctx;       // 协程山下文
+    char m_stack[DEFAULT_SIZE];     // 协程栈
+    State m_state;      // 协程状态
+    int m_id;       // 协程id
+    std::function<void()> m_cb;     // 回调函数
 };
 
 class CallerCoroutine {
