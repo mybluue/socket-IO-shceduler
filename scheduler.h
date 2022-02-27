bf82ddf4e4de7dca0a767c3ae02b79a669a9c8a7
mybluue <73818220+mybluue@users.noreply.github.com>
2022-02-27 12:14:27 +0800

    Update scheduler.h

--- scheduler.h
+++ scheduler.h
@@ -8,7 +8,7 @@
 #include <vector>
 #include <iostream>
 
-class TaskQueue {
+class TaskQueue {       // 线程安全的任务队列
 public:
     void enQueue(Coroutine* co) {
         std::unique_lock<std::mutex> lock(m_taskMutex);
@@ -28,15 +28,15 @@ public:
     }
 
 private:
-    std::queue<Coroutine*> m_queue;
-    std::mutex m_taskMutex;
+    std::queue<Coroutine*> m_queue;     // 协程队列
+    std::mutex m_taskMutex;     // 队列锁
 };
 
 
 class Scheduler {
 public:
     typedef std::shared_ptr<Scheduler> ptr;
-    class Worker {
+    class Worker {      // 线程入口仿函数
     public:
         Worker(Scheduler* sc);
         void operator()();
@@ -46,24 +46,21 @@ public:
         CallerCoroutine::ptr m_caller=nullptr;
     };
 protected:
-    virtual void idle();
-    virtual void tickle();
+    virtual void idle();        // 阻塞线程
+    virtual void tickle();      // 线程唤醒
 public:
-    Scheduler(int cnt);
+    Scheduler(int cnt);     // 指定线程数量
     ~Scheduler();
     void addTask(Coroutine* tk);
     // void schedule();
     void init();        // 创建线程并启动
-    void shutdown();
+    void shutdown();        // 关闭线程池
 protected:
-    TaskQueue *m_tasks;
-    std::vector<std::thread> m_threads;
-    std::mutex m_mutex;
+    TaskQueue *m_tasks;     // 当前调度器的任务队列
+    std::vector<std::thread> m_threads;     // 线程池
+    std::mutex m_mutex;     
     int thread_cnt;
-    bool is_shutdown;
+    bool is_shutdown;       // 是否关闭线程池
 };
-
-
-
 
 #endif
